081ffc38674b7d3519ce00ba058de5f8facfa5cb
RobertoGil <robert.dante3@gmail.com>
2021-07-06 16:46:09 +0200

    files práctica ec2

---
+++ script_start_ec2.py
@@ -0,0 +1,48 @@
+import boto3
+import sys
+import time
+
+
+ec2 = boto3.client('ec2')
+
+action = sys.argv[1] 
+instances = [sys.argv[2]] if len(sys.argv) > 2 else '.'
+
+def main():
+    if action == "stop":
+        stopInstance()
+    elif action == "start":
+        startInstance()
+    elif action == "list":
+        listInstances()
+    else:
+        print("Choose start + instanceId, stop + instanceId or list")
+        
+        
+def startInstance():
+    ec2.start_instances(InstanceIds=instances)
+    print("============================")
+    print('Try to start Ec2 instance...')
+    print("============================")
+    time.sleep(30)
+    
+def stopInstance():
+    ec2.stop_instances(InstanceIds=instances)
+    print("===========================")
+    print('Try to stop Ec2 instance...')
+    print("===========================")
+    time.sleep(30)
+    
+def listInstances():
+    response = ec2.describe_instances()
+    for reservation in response["Reservations"]:
+        for instance in reservation["Instances"]:
+            print(instance["InstanceId"])
+            print(instance["Tags"])
+            print(instance["Placement"])
+            print(instance["State"])
+            print("====================")
+       
+    
+if __name__ == '__main__':
+    main() 
---
+++ start_ec2_dag.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils import timezone
+from datetime import datetime, timedelta
+import pendulum
+
+local_tz = pendulum.timezone("Europe/Madrid")
+
+default_args = {
+        'owner': 'airflow',
+        'start_date': datetime(year=2021, month=6, day=29, tzinfo=local_tz),
+        'retries': 5,
+        'retry_delay': timedelta(minutes=5),
+        }
+
+dag = DAG(
+        'start_ec2',
+        default_args=default_args,
+        schedule_interval='22 7 * * *',
+        )
+
+create_command = 'python ~/scripts/start_stop_ec2.py start [InstanceId] '
+
+t1 = BashOperator(
+        task_id='start_instance',
+        bash_command=create_command,
+        dag=dag,
+        )
+
+
+~ 
